90966ee826ac80e2cdfda4b812377c0aaf155f50
zhannatk <zhanna.kindgirl@gmail.com>
2023-04-28 03:28:21 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+
+// public class Main {
+//    public static void main(String[] args) {
+//        Integer value = 33;
+//        System.out.println(value);
+//        changeValue(value);
+//        System.out.println(value);
+//    }
+//
+//    public static void changeValue(Integer value) {
+//        value = 22;
+//        System.out.println(value);
+//    }
+//}
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int value = 33;
+        System.out.println(value);
+        changeValue(value);
+        System.out.println(value);
+    }
+
+    public static void changeValue(int value) {
+        value = 22;
+        System.out.println(value);
     }
 }
